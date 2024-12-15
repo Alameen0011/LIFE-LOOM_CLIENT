d206dd22107433b1af64d1e0f0451731b6f3fd15
Al Ameen <sayedalameen1981@gmail.com>
2024-12-15 19:31:22 +0530

    deployment fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
 export default {
     darkMode: ["class"],
     content: [
@@ -84,5 +85,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
